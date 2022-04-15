083e93c92db7a7bac4c07c1e12d42c2cd7f8c258
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-04-15 14:13:58 +0300

    update class Counter, add button to do steps

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -6,16 +6,6 @@ const button = document.getElementById('button');
 const tablePosition = table.getBoundingClientRect();
 const ctx = canvas.getContext('2d');
 
-//import counter images
-const counterImages = [];
-for (let i = 1; i <= 10; i++) {
-  const counter = new Image();
-  counter.src = `counter-images/counter${i}.png`;
-  counterImages.push(counter);
-}
-const counterW = 41;
-const counterH = 70;
-
 //calculate cell centers
 const cells = {};
 const tableSize = 25;
@@ -31,28 +21,33 @@ for (let i = 1; i <= tableSize; i++) {
 class Counter {
   constructor() {
     this.score = 1;
-    this.number = Math.floor(Math.random() * counterImages.length);
-    this.image = counterImages[this.number];
-    this.draw();
+    this.number = Math.floor(Math.random() * 10) + 1;
+  }
+  set number(i) {
+    this.image = new Image();
+    this.image.src = `counter-images/counter${i}.png`;
+    this.image.onload = () => {
+      this.width = this.image.naturalWidth;
+      this.height = this.image.naturalHeight;
+      this.draw();
+    };
   }
   draw() {
-    this.image.onload = () =>
-      ctx.drawImage(this.image,
-        cells.c1.x - counterW / 2,
-        cells.c1.y - counterH / 2);
+    const coord = cells[`c${this.score}`];
+    ctx.drawImage(this.image,
+      coord.x - this.width / 2,
+      coord.y - this.height / 2);
   }
   step() {
     this.score += Math.floor(Math.random() * 6) + 1;
-    const coord = cells[`c${this.score}`];
-    this.image.onload = () =>
-      ctx.drawImage(this.image,
-        coord.x - counterW / 2,
-        coord.y - counterH / 2);
+    this.draw();
   }
 }
 
 const counter1 = new Counter();
-counter1.step();
-console.log('1:' + counter1.score);
-counter1.step();
-console.log('2:' + counter1.score);
+
+button.onclick = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  counter1.step();
+  console.log(counter1.score);
+};
